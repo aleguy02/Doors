dfa77718bb12cd91b1efc210f832ea7a18c1fa10
Ale V <alejandro.the.villate@gmail.com>
2025-02-08 16:25:38 -0500

    First commit
    Set up nativewind

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View, Button } from 'react-native';
+import './globals.css';
+
+export default function Index() {
+  return (
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //   }}
+    // >
+    //   <Button title="Link Google Drive Folder" />
+    //   <Text>Edit app/index.tsx to edit this screen.</Text>
+    // </View>
+
+    <View className="w-10 h-10 bg-blue-500"></View>
+  );
+}
