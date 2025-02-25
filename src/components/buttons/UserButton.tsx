1b49d6ed66d71344bcab762f1deef32957de6b88
aleguy02 <alejandro.the.villate@gmail.com>
2025-02-25 14:53:14 -0500

    Add redirect to user settings screen

--- src/components/buttons/UserButton.tsx
+++ src/components/buttons/UserButton.tsx
@@ -1,14 +1,18 @@
-import { Image, Pressable, Text } from 'react-native';
+import { Image, Pressable } from 'react-native';
+import { useRouter } from 'expo-router';
 
 const UserButton = () => {
+  const router = useRouter();
+  const redirectToUserScreen = () => {
+    router.push('/(user)');
+  };
+
   return (
-    <Pressable
-      onPress={() => {
-        console.log('Pressed!');
-      }}
-    >
-      {/* <Image source={require('../../../assets/icons/User_Cirle.png')} /> */}
-      <Image source={require('../../../assets/icons/User_Circle.png')} />
+    <Pressable className="flex" onPress={redirectToUserScreen}>
+      <Image
+        className="w-12 h-12"
+        source={require('../../../assets/icons/User_Circle.png')}
+      />
     </Pressable>
   );
 };
