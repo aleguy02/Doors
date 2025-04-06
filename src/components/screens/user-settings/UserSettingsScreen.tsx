d4b9df2ccc647b896dbdd4c2de5c5ef561608e8e
aleguy02 <alejandro.the.villate@gmail.com>
2025-04-06 18:42:30 -0400

    Remove commented-out code

--- src/components/screens/user-settings/UserSettingsScreen.tsx
+++ src/components/screens/user-settings/UserSettingsScreen.tsx
@@ -15,9 +15,8 @@ import CreateBandModal from './CreateBandModal';
 
 const UserSettingsScreen = () => {
   const { signoutUser } = useAuth();
-  // modal visibility is passed as a prop to `CreateBandModal` -> visibility can be toggled in and out of the component
+  // modal visibility is passed as a prop to `CreateBandModal` so visibility can be toggled in and out of the component
   const [createModalVisible, setCreateModalVisible] = useState(false);
-  // const [bandName, setBandName] = useState('');
 
   return (
     <ScrollView>
@@ -25,53 +24,6 @@ const UserSettingsScreen = () => {
         modalVisible={createModalVisible}
         setModalVisible={setCreateModalVisible}
       />
-      {/* <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-          console.log('on request close');
-        }}
-      >
-        <View className="flex-1 justify-center items-center">
-          <View className="w-3/4 p-10 bg-white rounded-lg shadow-lg items-center">
-            <Text className="text-lg font-semibold mb-2 self-start">
-              Band Name
-            </Text>
-            <TextInput
-              className="border border-gray-300 rounded p-2 mb-4 w-full"
-              maxLength={50}
-              placeholder="Band name"
-              value={bandName}
-              onChangeText={setBandName}
-            />
-
-            <CustomButton
-              text="Confirm"
-              onPress={() => {
-                if (!bandName) {
-                  Alert.alert('Band name required');
-                } else {
-                  // generate band object with unique band ID (define custom class for Band?)
-                  // link band object to user
-                }
-              }}
-            />
-
-            {/* Close modal }
-            <Button
-              title="Cancel"
-              onPress={() => {
-                setBandName('');
-                setModalVisible(!modalVisible);
-              }}
-            />
-          </View>
-        </View>
-      </Modal> */}
-
       <View className="flex-1 gap-8 justify-start items-center py-10">
         <CustomButton
           text="Create Band"
