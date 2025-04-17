c5696ad2e45bf92f45e349321c11921284cb727f
aleguy02 <alejandro.the.villate@gmail.com>
2025-04-17 17:08:10 -0400

    Move Auth screen into component in src

---
+++ src/components/screens/AuthScreen.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import {
+  TextInput,
+  ActivityIndicator,
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+
+import { useAuth } from '@/src/contexts/AuthContext';
+
+/* I recommend Simon Grimm's "Super Easy React Native AUTHENTICATION with Firebase 🔒" on YouTube to get started
+ * with authentication in React Native
+ */
+const AuthScreen = () => {
+  const { registerUser, loginUser, isLoading } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <ScrollView>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        className="flex-1 gap-8 justify-start items-center py-10"
+      >
+        <TextInput
+          className="w-4/5 p-4 border border-gray-300 rounded-md"
+          placeholder="Email"
+          autoCapitalize="none"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+        />
+        <TextInput
+          className="w-4/5 p-4 border border-gray-300 rounded-md"
+          placeholder="Password"
+          autoCapitalize="none"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          secureTextEntry={true}
+        />
+
+        {isLoading ? (
+          <ActivityIndicator size="large"></ActivityIndicator>
+        ) : (
+          <>
+            <Button title="Log In" onPress={() => loginUser(email, password)} />
+            <Button
+              title="Sign Up"
+              onPress={() => registerUser(email, password)}
+            />
+          </>
+        )}
+      </KeyboardAvoidingView>
+    </ScrollView>
+  );
+};
+
+export default AuthScreen;
