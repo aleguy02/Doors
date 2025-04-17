8977f607024114ce0702746cb4065d877bbf2af1
aleguy02 <alejandro.the.villate@gmail.com>
2025-04-17 02:02:02 -0400

    Update jest unit test suite

--- src/services/authService.test.ts
+++ src/services/authService.test.ts
@@ -1,13 +1,20 @@
-import { createNewUserService } from './authService';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { loginExistingUserService, createNewUserService } from './authService';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 import { setDoc, doc } from 'firebase/firestore';
 
+/* ==== SETUP ==== */
 /* First, jest parses this file and sees that we're mocking firebase auth.
  * Then, when the file is compiled/executed, the import from firebase auth gets overriden to resolve to this mock object
+ * So, our tests call a service function that calls these Firebase functions, but the ACTUAL function is replaced with
+ * this here implementation of the function
  */
 jest.mock('firebase/auth', () => {
   return {
     createUserWithEmailAndPassword: jest.fn(),
+    signInWithEmailAndPassword: jest.fn(),
     getReactNativePersistence: jest.fn(),
     initializeAuth: () => {
       return true;
@@ -24,15 +31,21 @@ jest.mock('firebase/firestore', () => {
   };
 });
 
+// This prevents different tests from interfering with eachother
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
 const mock_user = { uid: '123' };
 const mockument = {}; // as DocumentReference<DocumentData, DocumentData>;
 
+/* ==== TESTS ==== */
 describe('createNewUserService', () => {
-  test('Create a new user and store user data in Firestore', async () => {
+  test('Create new user and store user data in Firestore', async () => {
     (createUserWithEmailAndPassword as jest.Mock).mockResolvedValue({
       user: mock_user,
     });
-    (doc as jest.Mock).mockReturnValue(mockument);
+    (doc as jest.Mock).mockReturnValue(mockument); // I'm so funny
     (setDoc as jest.Mock).mockResolvedValue(null);
 
     const service_response = await createNewUserService(
@@ -55,5 +68,47 @@ describe('createNewUserService', () => {
       band_names: [],
     });
     expect(service_response).toBe(mock_user);
+  });
+
+  test('Attempt invalid email or password', async () => {
+    (createUserWithEmailAndPassword as jest.Mock).mockRejectedValue(
+      new Error('Invalid email')
+    );
+
+    await expect(createNewUserService('badEmail', 'password')).rejects.toThrow(
+      Error('Invalid email')
+    );
+    expect(doc).not.toHaveBeenCalled();
+    expect(setDoc).not.toHaveBeenCalled();
+  });
+});
+
+describe('loginExistingUserService', () => {
+  test('Login user', async () => {
+    (signInWithEmailAndPassword as jest.Mock).mockResolvedValue({
+      user: mock_user,
+    });
+
+    const service_response = await loginExistingUserService(
+      'test@gmail.com',
+      'password1'
+    );
+
+    expect(signInWithEmailAndPassword).toHaveBeenCalledWith(
+      expect.anything(), // firebase Auth
+      'test@gmail.com',
+      'password1'
+    );
+    expect(service_response).toBe(mock_user);
+  });
+
+  test('Attempt invalid email or password', async () => {
+    (signInWithEmailAndPassword as jest.Mock).mockRejectedValue(
+      new Error('Invalid email')
+    );
+
+    await expect(
+      loginExistingUserService('badEmail', 'password')
+    ).rejects.toThrow(Error('Invalid email'));
   });
 });
