eae2075dc169bda4c444725cfaedcb999ff977fa
aleguy02 <alejandro.the.villate@gmail.com>
2025-04-20 00:20:20 -0400

    Style ManageBands.tsx

--- src/components/screens/ManageBandsScreen.tsx
+++ src/components/screens/ManageBandsScreen.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { ScrollView, View } from 'react-native';
+
+import CustomButton from '../buttons/CustomButton';
+
+const dummy_data = { gnomefest: 'band123', 'Idle Hands': 'another123' };
+
+const button_map = Object.entries(dummy_data).map(([name, id]) => {
+  return (
+    <CustomButton key={name} variant={'long'} text={name} onPress={() => {}} />
+  );
+});
 
 export default function ManageBandsScreen() {
-  return <Text>Manage Bands</Text>;
+  return (
+    <ScrollView>
+      <View className="flex-1 gap-8 justify-start items-center py-10">
+        {button_map}
+      </View>
+    </ScrollView>
+  );
 }
