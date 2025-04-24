5fd8c125799e7b08e629ba7007daa5b2ae49b165
aleguy02 <alejandro.the.villate@gmail.com>
2025-04-24 14:39:13 -0400

    Create get band services

--- src/services/bandService.ts
+++ src/services/bandService.ts
@@ -43,10 +43,68 @@ export async function createNewBandService(
   });
 
   await updateDoc(docRef, {
-    // band_names: arrayUnion(...userPayload.band_names),
-    // band_ids: arrayUnion(...userPayload.band_ids),
     [`bands.${bandName}`]: bandDoc.id,
   });
 
   return bandDoc.id;
+}
+
+export async function getBandIDs(
+  fireStoreDB: any,
+  user: User
+): Promise<Record<string, string>> {
+  if (!user || !user.email) {
+    throw new Error('User is invalid');
+  }
+
+  // Get user document
+  const docRef = doc(fireStoreDB, 'users', user.uid);
+  const docSnap = await getDoc(docRef);
+  if (!docSnap.exists()) {
+    throw new Error('User doc not found');
+  }
+
+  // getIDs
+  const bands: Record<string, string> = docSnap.data().bands;
+  return bands;
+}
+
+export async function getBandInfo(
+  fireStoreDB: any,
+  ids: string[]
+): Promise<Record<string, FirestoreBandType>> {
+  return await arrayToRecordAsync<FirestoreBandType>(ids, async (id) => {
+    const docRef = doc(fireStoreDB, 'bands', id);
+    const docSnap = await getDoc(docRef);
+    if (!docSnap.exists()) {
+      throw new Error('User doc not found');
+    }
+    return {
+      name: docSnap.get('name'),
+      owner: docSnap.get('owner'),
+      members: docSnap.get('members'),
+    } as FirestoreBandType;
+  });
+}
+
+// helper function
+// function arrayToRecord<T>(
+//   keys: string[],
+//   callback: (key: string) => T
+// ): Record<string, T> {
+//   return keys.reduce((acc, curr) => {
+//     acc[curr] = callback(curr);
+//     return acc;
+//   }, {} as Record<string, T>);
+// }
+
+async function arrayToRecordAsync<T>(
+  keys: string[],
+  callback: (key: string) => Promise<T>
+): Promise<Record<string, T>> {
+  const entries = await Promise.all(
+    keys.map(async (key) => [key, await callback(key)] as const)
+  );
+
+  return Object.fromEntries(entries) as Record<string, T>;
 }
