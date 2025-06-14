03fa4089e309032704c8ac610d2ab37975b1f49b
aleguy02 <alejandro.the.villate@gmail.com>
2025-06-14 11:26:18 -0400

    Upgrade to Expo SDK 53

--- metro.config.js
+++ metro.config.js
@@ -5,5 +5,7 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 config.resolver.sourceExts.push('cjs');
+// upgrading to Expo SDK 53 broke Firebase, add this line to fix
+config.resolver.unstable_enablePackageExports = false;
 
 module.exports = withNativeWind(config, { input: './app/globals.css' });
